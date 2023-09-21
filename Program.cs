53b29a090e2497cd41a3cd40847b04aaed638418
SindreMA <SindreMA@gmail.com>
2023-09-21 23:19:51 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTSC_MinimizToggler
+{
+    class Program
+    {
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        private struct Windowplacement
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public Point ptMinPosition;
+            public Point ptMaxPosition;
+            public Rectangle rcNormalPosition;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string windowTitle);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref Windowplacement lpwndpl);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, ShowWindowEnum.Hide);
+
+            var pros = Process.GetProcessesByName("mstsc");
+            foreach (var pro in pros)
+            {
+                var wdwIntPtr = pro.MainWindowHandle;
+                Windowplacement placement = new Windowplacement();
+                GetWindowPlacement(wdwIntPtr, ref placement);
+
+                // Check if window is minimized
+                if (placement.showCmd == 2)
+                {
+                    //the window is hidden so we restore it
+                    ShowWindow(wdwIntPtr, ShowWindowEnum.Restore);
+                    SetForegroundWindow(wdwIntPtr);
+                } else
+                {
+                    ShowWindow(wdwIntPtr, ShowWindowEnum.ForceMinimized);
+                }
+              
+            }
+        }
+    }
+}
